32c8d9a07a3a69a700103349955196356f643c8d
Sergey Medvedev <medvedevsm1989@yandex.ru>
2021-03-13 20:33:33 +0300

    wf 27

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -56,10 +56,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram_api.wsgi.application'
 
-
-Database
-https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
